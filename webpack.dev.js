bd30608e613d0cfb8a6301b991b865e4e7780fb0
Adam Chandra Setianugraha <adamchandra890@gmail.com>
2021-11-02 12:30:25 +0700

    add web manifest

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,13 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
-const common = require('./webpack.common');
- 
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common");
+
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    host: "192.168.43.101", //your ip address
+    port: 8080,
+    disableHostCheck: true,
+    contentBase: path.resolve(__dirname, "dist"),
   },
 });
